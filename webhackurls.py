c6e62e828c267fd55b9a5e3abc8e0004f5708e14
S1rN3tZ <40497633+mathis2001@users.noreply.github.com>
2022-06-16 12:57:07 +0200

    Add files via upload

--- webhackurls.py
+++ webhackurls.py
@@ -2,7 +2,6 @@
 
 import requests
 import sys
-from sys import argv
 import argparse
 
 print('''
@@ -21,48 +20,44 @@ class bcolors:
 	RESET = '\033[0m'
 	INFO = '\033[94m'
 
-def parser():
-	parser = argparse.ArgumentParser()
-	parser.add_argument("-d", action='store_true', help="target domain (exp: target.com)")
-	parser.add_argument("-k", action='store_true', help="search for a specific extension or keyword (js, xml, json, pdf... or admin, login, dashboard...)")
-	parser.add_argument("-l", action='store_true', help="limit (number of links you want)")
-	parser.add_argument("-o", action='store_true', help="Output file name")
-	args = parser.parse_args()
-	print(args)
 
-def getopts(argv):
-	opts = {}  
-	while argv:
-		if argv[0][0] == '-':
-			try:
-				opts[argv[0]] = argv[1]  
-			except Exception:
-				parser()
-		argv = argv[1:] 
-	return opts
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--domain", help="target domain (exp: target.com)", type=str)
+parser.add_argument("-k", "--keyword", help="search for a specific extension or keyword (js, xml, json, pdf... or admin, login, dashboard...)", type=str)
+parser.add_argument("-l", "--limit", help="limit (number of links you want)", type=str)
+parser.add_argument("-o", "--output", help="Output file name", type=str)
+args = parser.parse_args()
+	
+
 
 def main():
-	myargs = getopts(argv)
 	url="https://web.archive.org/cdx/search?matchType=domain&collapse=urlkey&output=text&fl=original"
 
 	if len(sys.argv) < 2:
 		print(bcolors.FAIL+"[!] "+bcolors.RESET+"No target given.")
 		print(bcolors.INFO+"[*] "+bcolors.RESET+"usage: ./webhackurls -d target.com [-k keyword] [-l limit] [-o output]")
 		print(bcolors.INFO+"[*] "+bcolors.RESET+"help: ./webhackurls -h")
+		exit(0)
 	else:
-		if '-d' in myargs:
-			url = url+"&url="+myargs['-d']+"/"
-			if '-k' in myargs:
-				url=url+"&filter=urlkey:.*"+myargs['-k']
-			if '-l' in myargs:
-				url=url+"&limit="+myargs['-l']
+		if args.domain:
+			domain = args.domain
+			url = url+"&url="+domain+"/"
+			if args.keyword:
+				keyword=args.keyword
+				url=url+"&filter=urlkey:.*"+keyword
+			if args.limit:
+				limit=args.limit
+				url=url+"&limit="+limit
 		else:
 			print(bcolors.FAIL+"[!] "+bcolors.RESET+"No target given.")
+
+	print(bcolors.INFO+"[*] "+bcolors.RESET+"Processing your request... It can take few seconds.")
 	rq=requests.get(url)
 	print(rq.text)
 	
-	if '-o' in myargs:
-		log = open(myargs['-o'], "w")
+	if args.output:
+		file = args.output
+		log = open(file, "w")
 		log.write(rq.text)
 		log.close()
 try:
