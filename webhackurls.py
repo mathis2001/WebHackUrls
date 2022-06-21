bcd0a6a544cc14d9ed424bd01d771444eb4e2eac
S1rN3tZ <40497633+mathis2001@users.noreply.github.com>
2022-06-21 16:53:55 +0200

    Add files via upload

--- webhackurls.py
+++ webhackurls.py
@@ -3,6 +3,9 @@
 import requests
 import sys
 import argparse
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from time import sleep
 
 print('''
 
@@ -25,10 +28,29 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-d", "--domain", help="target domain (exp: target.com)", type=str)
 parser.add_argument("-k", "--keyword", help="search for a specific extension or keyword (js, xml, json, pdf... or admin, login, dashboard...)", type=str)
 parser.add_argument("-l", "--limit", help="limit (number of links you want)", type=str)
+parser.add_argument("-s", "--screenshot", help="take screenshot of each url", action="store_true")
 parser.add_argument("-o", "--output", help="Output file name", type=str)
 args = parser.parse_args()
 	
-
+def screenshot(urls):
+	options = Options()
+	options.add_argument('--headless')
+	driver = webdriver.Firefox(options=options)
+	driver.set_page_load_timeout(30)    
+	driver.maximize_window()
+	
+	lines=urls.split()
+	i=0
+	print(bcolors.OK+"[+] "+bcolors.RESET+"Screening urls...")
+	for url in lines:
+        	i=i+1
+        	cut=url.split('/')
+        	driver.get(url)
+        	sleep(1)
+        	driver.save_screenshot("screens/screen-"+str(cut[3])+"-"+str(i)+".png")
+        	print('.')
+	driver.quit()
+	print(bcolors.OK+"[+] "+bcolors.RESET+"done!")
 
 def main():
 	url="https://web.archive.org/cdx/search?matchType=domain&collapse=urlkey&output=text&fl=original"
@@ -54,7 +76,9 @@ def main():
 	print(bcolors.INFO+"[*] "+bcolors.RESET+"Processing your request... It can take few seconds.")
 	rq=requests.get(url)
 	print(rq.text)
-	
+
+	if args.screenshot:
+		screenshot(rq.text)
 	if args.output:
 		file = args.output
 		log = open(file, "w")
